79896463da5b064fae3db357c00e860a7db74950
oddlyspaced <oddlyspaced1@gmail.com>
2020-03-25 01:44:45 +0530

    Telethon: Implement fetching of fake mail

--- haxx.py
+++ haxx.py
@@ -6,4 +6,27 @@ api_hash = 'b6bfa22e694d119dc13294062f63e6fd'
 
 # Initialising the Telegram Client
 client = TelegramClient("session", api_id, api_hash)
+
+# Constants for telegram side
+generate_count = 0
+id_fakemailbot = 'fakemailbot'
+new_mail_string = 'Your new fake mail id'
+fake_mail = ''
+fake_mail_domain = '@hi2.in'
+
+@client.on(events.NewMessage)
+async def response_handler(event):
+    # new fake mail
+    global fake_mail
+    if new_mail_string in event.raw_text:
+        words = str(event.raw_text).split(" ")
+        for word in words:
+            if fake_mail_domain in word:
+                fake_mail = word
+
+def generate_email():
+    client.send_message(id_fakemailbot, "/generate")
+
 client.start()
+generate_email()
+client.run_until_disconnected()
